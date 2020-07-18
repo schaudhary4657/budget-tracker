163be0eccc0ab65c7f12b018f893ef00b2da5ebf
Sapana Chaudhary <sapana.chy23@gmail.com>
2020-07-17 17:49:00 -0700

    added db.js file for offline function

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
 const FILES_TO_CACHE = [
   "/",
+  "/db.js",
   "/index.html",
   "/styles.css",
   "/index.js",
@@ -8,7 +9,7 @@ const FILES_TO_CACHE = [
   "/manifest.json",
 ];
 
-const STATIC_CACHE = "static-cache-v1";
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
